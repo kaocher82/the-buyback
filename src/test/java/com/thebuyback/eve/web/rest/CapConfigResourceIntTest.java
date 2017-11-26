1bf189de836f907321499c72b73b23a999cae976
bahrmichael <mbahr92@gmail.com>
2017-11-26 23:15:46 +0100

    testfix

--- src/test/java/com/thebuyback/eve/web/rest/CapConfigResourceIntTest.java
+++ src/test/java/com/thebuyback/eve/web/rest/CapConfigResourceIntTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
+@ContextConfiguration(initializers = EnvironmentTestConfiguration.class)
 public class CapConfigResourceIntTest {
 
     private static final Integer DEFAULT_TYPE_ID = 1;
